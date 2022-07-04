012387196d53b89e70ec24f3d8e868f6d6e1e626
mchemlal <michel.chemlal@gmail.com>
2022-07-04 19:49:29 +0200

    google provider's set up

--- client/index.php
+++ client/index.php
@@ -43,7 +43,16 @@ function login()
         "scope" => "connections",
         "state" => bin2hex(random_bytes(16))
     ));
-    echo "<a href=\"https://discord.com/api/oauth2/authorize?{$queryParams}\">Se connecter via Discord</a>";
+    echo "<a href=\"https://discord.com/api/oauth2/authorize?{$queryParams}\">Se connecter via Discord</a><br>";
+
+    $queryParams= http_build_query(array(
+        "client_id" => "727904560873-1im2ajo9g79bp0tugf7nad4t2pvpkrfs.apps.googleusercontent.com",
+        "redirect_uri" => "http://localhost:8081/go_callback",
+        "response_type" => "code",
+        "scope" => "profile",
+        "state" => bin2hex(random_bytes(16))
+    ));
+    echo "<a href=\"https://accounts.google.com/o/oauth2/auth?{$queryParams}\">Se connecter via Google</a>";
 }
 
 function callback()
@@ -220,6 +229,53 @@ function dscallback()
     echo "Hello {$result['username']}";
 }
 
+    /*========= PROVIDER GOOGLE =========*/
+    function gocallback()
+    {
+        $specifParams = [
+            "grant_type" => "authorization_code",
+            "code" => $_GET["code"]
+        ];
+
+        $clientId = "727904560873-1im2ajo9g79bp0tugf7nad4t2pvpkrfs.apps.googleusercontent.com";
+        $clientSecret = "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";
+        $redirectUri = "http://localhost:8081/go_callback";
+
+        $data = http_build_query(array_merge([
+            "redirect_uri" => $redirectUri,
+            "client_id" => $clientId,
+            "client_secret" => $clientSecret,
+        ], $specifParams));
+
+        $url = "https://oauth2.googleapis.com/token";
+        $options = array(
+            'http' => array(
+            'method' => 'POST',
+            'header' => "Content-Type: application/x-www-form-urlencoded",
+            'content' => $data
+            )
+        );
+
+        $context = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        $result = json_decode($result, true);
+        $accessToken = $result['access_token'];
+
+        $url = "https://www.googleapis.com/oauth2/v1/userinfo";
+        $options = array(
+                'http' => array(
+                'method' => 'GET',
+                'header' => 'Authorization: Bearer ' . $accessToken
+            )
+        );
+
+        $context = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        $result = json_decode($result, true);
+        //var_dump($result);
+        echo "Hello {$result['name']}";
+    }
+
 $route = $_SERVER['REQUEST_URI'];
 switch (strtok($route, "?")) {
     case '/login':
@@ -236,6 +292,9 @@ switch (strtok($route, "?")) {
         break;
     case '/ds_callback':
         dscallback();
+        break;
+    case '/go_callback':
+        gocallback();
         break;
     default:
         echo '404';
