5127f2f01a283c6cb81ff6ba9ee32607468e5f1d
Choetsu <samy.amh94@gmail.com>
2022-07-15 15:08:00 +0200

    Set up of the front end layer with morade team mate

--- client/index.php
+++ client/index.php
@@ -1,6 +1,19 @@
 <?php
-
 namespace App;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SDK - OAUTH</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php
 
 use InvalidArgumentException;
 
@@ -20,70 +33,84 @@ spl_autoload_register('App\myAutoloader');
 
 function home()
 {
-    echo "<a href=\"/login\">Se connecter</a>";
+    echo "<div class='seco'><a href=\"/login\" class='btn-login'>Navigation vers SDK OAUTH</a></div>";
 }
 
 function login()
 {
+    $serv = new providers\Serv(CLIENT_ID_SERV, CLIENT_SECRET_SERV, REDIRECT_URI_SERV, 'read,write');
+    $facebook = new providers\Facebook(CLIENT_ID_FB, CLIENT_SECRET_FB, REDIRECT_URI_FB, 'public_profile,email');
+    $github = new providers\Github(CLIENT_ID_GIT, CLIENT_SECRET_GIT, REDIRECT_URI_GIT, 'user', [], 'github');
+    $discord = new providers\Discord(CLIENT_ID_DS, CLIENT_SECRET_DS, REDIRECT_URI_DS, 'connections');
+    $google = new providers\Google(CLIENT_ID_GO, CLIENT_SECRET_GO, REDIRECT_URI_GO, 'profile');
+
 
     echo "
-        <form action='callback' method='POST'>
-            <input type='text' name='username'>
-            <input type='text' name='password'>
-            <input type='submit' value='Login'>
+        <form action='callback' method='POST' id='login-form'>
+            <div class='heading'>SDK OAUTH</div>
+            <div class='left'> 
+                <label for='username'>Nom d'utilisateur</label> <br>           
+                <input type='text' name='username'> <br>
+                <label for='password'>Mot de passe</label> <br>
+                <input type='text' name='password'> <br>
+                <input type='submit' value='Connexion'>
+            </div>
+            <div class='right'>
+                <div class='connect'>Se connecter avec :</div>
+                <a href={$serv->getAuthUrl()} class='btn'>
+                    SERV
+                </a> <br>
+                <a href={$facebook->getAuthUrl()} class='btn'>
+                    FACEBOOK
+                </a> <br>
+                <a href={$github->getAuthUrl()} class='btn'>
+                    GITHUB
+                </a> <br>
+                <a href={$discord->getAuthUrl()} class='btn'>
+                    DISCORD
+                </a> <br>
+                <a href={$google->getAuthUrl()} class='btn'>
+                    GOOGLE
+                </a> <br>
+            </div>
         </form>
     ";
-
-    $serv = new providers\Serv(CLIENT_ID_SERV, CLIENT_SECRET_SERV, REDIRECT_URI_SERV, 'read,write');
-    echo "<a href={$serv->getAuthUrl()}>Se connecter via Oauth Server</a><br>";
-
-    $facebook = new providers\Facebook(CLIENT_ID_FB, CLIENT_SECRET_FB, REDIRECT_URI_FB, 'public_profile,email');
-    echo "<a href={$facebook->getAuthUrl()}>Se connecter via Facebook</a><br>";
-
-    $github = new providers\Github(CLIENT_ID_GIT, CLIENT_SECRET_GIT, REDIRECT_URI_GIT, 'user', [], 'github');
-    echo "<a href={$github->getAuthUrl()}>Se connecter via Github</a><br>";
-
-    $discord = new providers\Discord(CLIENT_ID_DS, CLIENT_SECRET_DS, REDIRECT_URI_DS, 'connections');
-    echo "<a href={$discord->getAuthUrl()}>Se connecter via Discord</a><br>";
-
-    $google = new providers\Google(CLIENT_ID_GO, CLIENT_SECRET_GO, REDIRECT_URI_GO, 'profile');
-    echo "<a href={$google->getAuthUrl()}>Se connecter via Google</a><br>";    
 }
 
 /*========= PROVIDER SERV =========*/
 function callback()
 {
     $serv = new providers\Serv(CLIENT_ID_SERV, CLIENT_SECRET_SERV, REDIRECT_URI_SERV, 'read,write');
-    echo $serv->getUser($serv->getAccessToken(empty($_GET['code']) ? $_GET['code'] = 'test' : $_GET['code']));
+    echo "<p>Bienvenue ".$serv->getUser($serv->getAccessToken(empty($_GET['code']) ? $_GET['code'] = 'test' : $_GET['code']))."</p>";
 }
 
 /*========= PROVIDER FACEBOOK =========*/
 function fbcallback()
 {
     $facebook = new providers\Facebook(CLIENT_ID_FB, CLIENT_SECRET_FB, REDIRECT_URI_FB, 'public_profile,email');
-    echo $facebook->getUser($facebook->getAccessToken($_GET['code']));
+    echo "<p>Bienvenue ".$facebook->getUser($facebook->getAccessToken($_GET['code']))."</p>";
 }
 
 /*========= PROVIDER GITHUB =========*/
 function gitcallback()
 {
     $github = new providers\Github(CLIENT_ID_GIT, CLIENT_SECRET_GIT, REDIRECT_URI_GIT, 'user', [], 'github');
-    echo $github->getUser($github->getAccessToken($_GET['code']));
+    echo "<p>Bienvenue ".$github->getUser($github->getAccessToken($_GET['code']))."</p>";
 }
 
 
 /*========= PROVIDER DISCORD =========*/
 function dscallback()
 {
-    $discord = new providers\Discord(CLIENT_ID_DS, CLIENT_SECRET_DS, REDIRECT_URI_DS, 'connections');
-    echo $discord->getUser($discord->getAccessToken($_GET['code']));   
+    $discord = new providers\Discord(CLIENT_ID_DS, CLIENT_SECRET_DS, REDIRECT_URI_DS, 'connections'); 
+    echo "<p>Bienvenue ".$discord->getUser($discord->getAccessToken($_GET['code']))."</p>";
 }
 
 /*========= PROVIDER GOOGLE =========*/
 function gocallback()
 {
     $google = new providers\Google(CLIENT_ID_GO, CLIENT_SECRET_GO, REDIRECT_URI_GO, 'profile');
-    echo $google->getUser($google->getAccessToken($_GET['code']));
+    echo "<p>Bienvenue ".$google->getUser($google->getAccessToken($_GET['code']))."</p>";
 }
 
 $route = $_SERVER['REQUEST_URI'];
@@ -114,3 +141,6 @@ switch (strtok($route, "?")) {
         break;
 }
 ?>
+
+</body>
+</html>
